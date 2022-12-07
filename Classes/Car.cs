f2890ae02e8732a3c989c869e2e41a37d9402b90
Kassandra Johnson <kassandra.m.johnson@gmail.com>
2022-12-07 14:46:12 -0500

    completed Exercise 1

---
+++ Classes/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        //In the Car class:
+        //Create a Make property of type string that is public
+        //Create a Model property of type string that is public
+        //Create a Year property of type int that is public
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Classes
+﻿using Microsoft.VisualBasic;
+using System.Reflection;
+using System.Xml.Schema;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create a class named Car - make sure it is public
+            
+            //In the Main method:
+            //Create a new instance of the Car class
+            //Set values in the properties for the object
+            //Print the values of each property to the Console
+
+            Car car1 = new Car() { Make = "Toyota", Model = "Camry Hybrid", Year = 2007 };
+            Console.WriteLine($"car1 is a {car1.Year} {car1.Make} {car1.Model}");
+
         }
-    }
+}
 }
